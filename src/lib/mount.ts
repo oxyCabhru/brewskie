24626e0be0daad8a7a043792dce224e4202a0e29
oxyJinn <danielooxygen@gmail.com>
2023-06-13 10:23:04 +0300

    added vercel analytics

--- src/lib/mount.ts
+++ src/lib/mount.ts
@@ -1,3 +1,8 @@
 import Axios from 'axios';
 import { setupCache } from 'axios-cache-interceptor';
 export const axios = setupCache(Axios);
+
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+ 
+inject({ mode: dev ? 'development' : 'production' });
