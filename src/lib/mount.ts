e416de7d602b0546178ce2c0557c04185e8b67b8
oxyCabhru <danielooxygen@gmail.com>
2023-06-25 14:21:09 +0300

    disable vercel analytics

--- src/lib/mount.ts
+++ src/lib/mount.ts
@@ -2,7 +2,7 @@ import Axios from 'axios';
 import { setupCache } from 'axios-cache-interceptor';
 export const axios = setupCache(Axios);
 
-import { dev } from '$app/environment';
-import { inject } from '@vercel/analytics';
+// import { dev } from '$app/environment';
+// import { inject } from '@vercel/analytics';
  
-inject({ mode: dev ? 'development' : 'production' });
+// inject({ mode: dev ? 'development' : 'production' });
