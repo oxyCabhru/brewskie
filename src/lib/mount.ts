8c3cd41b0e0bc2599ead5dcd25562a60a46b8622
oxyCabhru <danielooxygen@gmail.com>
2023-06-26 11:49:30 +0300

    removed vercel analytics

--- src/lib/mount.ts
+++ src/lib/mount.ts
@@ -1,8 +1,3 @@
 import Axios from 'axios';
 import { setupCache } from 'axios-cache-interceptor';
 export const axios = setupCache(Axios);
-
-import { dev } from '$app/environment';
-import { inject } from '@vercel/analytics';
-
-inject({ mode: dev ? 'development' : 'production' });
