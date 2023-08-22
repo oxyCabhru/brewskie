e362585fb861cd298f85fdeaa926c13f595fc135
oxy <danielooxygen@gmail.com>
2023-08-22 12:47:23 +0300

    hotfix: git commit hash baked into the docker img

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import adapter from "@sveltejs/adapter-node";
-import * as child_process from 'child_process';
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
