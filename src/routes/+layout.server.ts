af4f73e88faf5cb45a9a10e0e4826d8546689ad6
oxy <danielooxygen@gmail.com>
2023-08-22 12:47:23 +0300

    hotfix: git commit hash baked into the docker img

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,7 +4,7 @@ import KeyvRedis from "@keyv/redis";
 import got from "got";
 export async function load() {
     const db_status = db instanceof KeyvRedis ? "redis" : "dev";
-    const revision = env.COMMIT_HASH || (await got("https://api.github.com/repos/oxycabhru/brewskie/commits/production").json() as any).sha as string; //im so sorry;
+    const revision = 'VITE_COMMIT_HASH';
     return {
         build_data: {
             db_status,
