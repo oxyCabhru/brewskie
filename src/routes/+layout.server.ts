6f2b2bf4799bb2bbd880d898ff5b2f49e4e101aa
oxy <danielooxygen@gmail.com>
2023-09-05 14:54:59 +0300

    commit hash hotfix

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,9 @@
 import { env } from "$env/dynamic/private";
 import db from "$lib/db.server";
 import KeyvRedis from "@keyv/redis";
-import got from "got";
 export async function load() {
     const db_status = db instanceof KeyvRedis ? "redis" : "dev";
-    const revision = 'VITE_COMMIT_HASH';
+    const revision = Buffer.from(JSON.parse('commit')).toString().trim();
     return {
         build_data: {
             db_status,
