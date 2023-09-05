5c49be0dde77210074c02a0f199d4e90a168e708
oxy <danielooxygen@gmail.com>
2023-09-05 14:48:07 +0300

    commit hash hotfix

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import * as child from "child_process";
-const VITE_COMMIT_HASH = child.execSync('git rev-parse HEAD').toString().trim();
+const VITE_COMMIT_HASH = child.execSync('git rev-parse HEAD');
 
 export default defineConfig({
   plugins: [sveltekit()],
   define: { 
-    "VITE_COMMIT_HASH": VITE_COMMIT_HASH
+    "commit": [...VITE_COMMIT_HASH]
    },
   css: {
     preprocessorOptions: {
