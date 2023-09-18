be78193e95c6870e53cba9279667f151e7a891ca
oxy <danielooxygen@gmail.com>
2023-09-18 14:36:14 +0300

    vercelify

--- src/lib/db.server.ts
+++ src/lib/db.server.ts
@@ -1,13 +1,5 @@
-import { building, dev } from "$app/environment";
 
-import KeyvRedis from "@keyv/redis";
 import Keyv from "keyv";
-let db: KeyvRedis<any> | Keyv;
-
-if (!building && !dev) {
-    db = new KeyvRedis("redis://redis-cluster:6379");
-} else {
-    db = new Keyv();
-}
-
+let db: Keyv;
+db = new Keyv();
 export default db;
